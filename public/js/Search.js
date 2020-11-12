aef8695c323dd5b40b5730d8dedd76d41614d1fe
Gerard Hubert <mikado842@gmail.com>
2020-11-12 01:24:13 +0100

    recherche de jeux en backoffice

--- public/js/Search.js
+++ public/js/Search.js
@@ -2,34 +2,90 @@ class Search {
     constructor() {
         this.userInput = document.querySelector('#game-search');
         this.searchButton = document.querySelector('.query_button');
-        this.titleElement = document.querySelector('#game_title');
-        this.imageElement = document.querySelector('#game_image');
+        this.resultsContainer = document.querySelector('.search_results');
+        this.checkboxes = document.getElementsByClassName('box');
+        this.queryMessageElement = document.getElementById('query_message');
+        this.reviewForm = document.querySelector('.review_form');
+
+        // Création des éléments pour insertion des résultats
 
         this.searchButton.addEventListener("click", () => {
-            this.getGames();
+            this.deletePreviousSearch();
         })
     }
 
-    // On réécupère d'abord la liste des jeux de l'API
+    deletePreviousSearch() {
+        if (this.resultsContainer.childNodes) {
+            const children = this.resultsContainer.childNodes;
+            console.log(children)
+            };
+        
+        this.search();
+    }
 
-    getGames() {
-        const research = this.userInput.value.toLowerCase().replace(/ /g, '-');
+    search() {
+        /**
+         * On vérifie d'abord la requete
+         * champs de recherche et 1 seule checkbox obligatoires
+         */
+
+        // on récupère un nom de plateform exploitable dans la requete (par les checkboxes)
+        const boxes = [];
+        for (let box of this.checkboxes) {
+            if (box.checked) {
+                boxes.push(box);
+            }
+        }
+
+        if (boxes.length !== 1) {
+            this.queryMessageElement.style.display = 'block';
+        } else {
+            this.queryMessageElement.style.display = 'none';
+            this.getGames(boxes[0].value);
+        }
+    }
+
+    getGames(platformId) {
+        //const research = this.userInput.value.toLowerCase().replace(/ /g, '-');
         const getData = async () => {
-            const response = await fetch("https://api.rawg.io/api/games/" + research + "?key=2d3f2baa156044ab91295ba0e044da14");
+            const response = await fetch("https://api.rawg.io/api/games?key=2d3f2baa156044ab91295ba0e044da14&search="
+                + this.userInput.value
+                + '&platforms='
+                + platformId
+                + '&exclude_additions');
             const games = await response.json();
-
-            this.showResults(games);
+            this.showResults(games.results);
         }
         getData();
     }
 
-    // on vérifie si le jeu recherché est dans la liste retournée
-
     showResults(games) {
-        console.log(games);
-        console.log(games.background_image);
-        this.titleElement.innerHTML = games.name;
-        this.imageElement.setAttribute('src', games.background_image);
+
+        if (games.length !== 0) {
+
+            this.resultsContainer.className = 'search_results_on';
+            
+            for (let game of games) {
+                
+                let resultCard = document.createElement('div')
+                resultCard.setAttribute('class', 'result_thumbnail');
+
+                let titleResult = document.createElement('h4');
+                titleResult.setAttribute('class', 'game_title');
+                titleResult.innerHTML = game.name;
+                resultCard.appendChild(titleResult);
+
+                let imageResult = document.createElement('img');
+                imageResult.setAttribute('class', 'game_image');
+                imageResult.src = game.background_image;
+                resultCard.appendChild(imageResult);
+
+                this.resultsContainer.appendChild(resultCard);
+            }
+        }
+            else {
+                alert('aucun resultat');
+            }
     }
 }
 
